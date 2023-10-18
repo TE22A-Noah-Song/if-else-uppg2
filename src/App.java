68f8fb9f3347042d9b66949c19dd3a61a191b53d
Noah Söng <noah.song@elev.ga.ntig.se>
2023-10-18 10:28:55 +0200

    gjort b och c

--- src/App.java
+++ src/App.java
@@ -1,19 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int litetTal=10;
-        int stortTal=1000;
+        // int litetTal=10;
+        // int stortTal=1000;
 
-        if(litetTal<1000)
+        // if(litetTal<1000)
+        // {
+        // System.out.println(litetTal +"är mindre än"+stortTal);
+        // }
+        // else
+        // {
+        // System.out.println(litetTal +"är mindre än"+stortTal);
+        // }
+        // System.out.println("litetTal="+litetTal+", stortTal="+stortTal);
+
+        //Man kan inte sätta en variabel inuti en if sats
+       
+        int tal1=10;
+        int tal2=5;
+        if(tal2>tal1)
         {
-        System.out.println(litetTal +"är mindre än"+stortTal);
+        System.out.println(tal2+" är större än "+tal1);
         }
         else
         {
-        System.out.println(litetTal +"är mindre än"+stortTal);
+            System.out.println(tal2+" är mindre än "+tal1);
         }
-        System.out.println("litetTal="+litetTal+", stortTal="+stortTal);
+        
 
-        //Man kan inte sätta en variabel inuti en if sats
         
     }
 }
