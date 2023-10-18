25acaea0e69ff6490d5f6e240e44b9f21445fd5a
Noah Söng <noah.song@elev.ga.ntig.se>
2023-10-18 10:14:23 +0200

    gjort a)

---
+++ src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int litetTal=10;
+        int stortTal=1000;
+
+        if(litetTal<1000)
+        {
+        System.out.println(litetTal +"är mindre än"+stortTal);
+        }
+        else
+        {
+        System.out.println(litetTal +"är mindre än"+stortTal);
+        }
+        System.out.println("litetTal="+litetTal+", stortTal="+stortTal);
+
+        //Man kan inte sätta en variabel inuti en if sats
+        
+    }
+}
+
