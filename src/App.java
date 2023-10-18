bab2730a2859a6518d891e32e9001f05d78c70d1
Noah Söng <noah.song@elev.ga.ntig.se>
2023-10-18 10:54:37 +0200

    gjort d)

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         // int litetTal=10;
@@ -15,18 +17,38 @@ public class App {
 
         //Man kan inte sätta en variabel inuti en if sats
        
-        int tal1=10;
-        int tal2=5;
-        if(tal2>tal1)
-        {
-        System.out.println(tal2+" är större än "+tal1);
-        }
-        else
-        {
-            System.out.println(tal2+" är mindre än "+tal1);
-        }
+        // int tal1=10;
+        // int tal2=5;
+        // if(tal2>tal1)
+        // {
+        // System.out.println(tal2+" är större än "+tal1);
+        // }
+        // else
+        // {
+        //     System.out.println(tal2+" är mindre än "+tal1);
+        // }
         
+        Scanner t=new Scanner(System.in);
+        System.out.println("Säg ett heltal 1-24");
+        int tid=t.nextInt();
+        if(tid>=23)
+        {
+        System.out.println("dags att gå och lägga sig.");
+        }
 
+        else if(tid>=12)
+        {
+        System.out.println("dags att äta");
+        }
+
+        else if(tid>=7)
+        {
+        System.out.println("dags att gå upp.");
+        }
+        else if(tid<7)
+        {
+        System.out.println("dags att gå upp.");
+        }
         
     }
 }
