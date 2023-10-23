e2a4f916d6d57959ac3d991cb9db774f2926da0e
Noah Söng <noah.song@elev.ga.ntig.se>
2023-10-23 08:49:27 +0200

    gjort e)

--- src/App.java
+++ src/App.java
@@ -28,28 +28,42 @@ public class App {
         //     System.out.println(tal2+" är mindre än "+tal1);
         // }
         
-        Scanner t=new Scanner(System.in);
-        System.out.println("Säg ett heltal 1-24");
-        int tid=t.nextInt();
-        if(tid>=23)
-        {
-        System.out.println("dags att gå och lägga sig.");
-        }
+        // Scanner t=new Scanner(System.in);
+        // System.out.println("Säg ett heltal 1-24");
+        // int tid=t.nextInt();
+        // if(tid>=23)
+        // {
+        // System.out.println("dags att gå och lägga sig.");
+        // }
 
-        else if(tid>=12)
-        {
-        System.out.println("dags att äta");
-        }
+        // else if(tid>=12)
+        // {
+        // System.out.println("dags att äta");
+        // }
 
-        else if(tid>=7)
-        {
-        System.out.println("dags att gå upp.");
-        }
-        else if(tid<7)
-        {
-        System.out.println("dags att gå upp.");
-        }
+        // else if(tid>=7)
+        // {
+        // System.out.println("dags att gå upp.");
+        // }
+        // else if(tid<7)
+        // {
+        // System.out.println("dags att gå upp.");
+        // }
         
+        Scanner t=new Scanner(System.in);
+        System.out.println("Är du medlem i klubben? Ja/Nej");
+        String medlem=t.nextLine();
+        System.out.println("Är du över 18? Ja/Nej");
+        String ålder=t.nextLine();
+        if(ålder.equalsIgnoreCase("Ja")||medlem.equalsIgnoreCase("Ja"))
+        {
+        System.out.println("Du får komma in!");
+        }
+        else
+        {
+        System.out.println("Du får inte komma in!");
+        }
+
     }
 }
 
